9c83bc977b4f6bbe57d1f9e7272e3ba246d75592
Naveen Varma <naveenvarmatrio@gmail.com>
2017-10-13 12:16:38 +0530

    Update WelcomeActivity.java

--- app/src/main/java/com/nis/neevan/sharedpreferencesdemo/WelcomeActivity.java
+++ app/src/main/java/com/nis/neevan/sharedpreferencesdemo/WelcomeActivity.java
@@ -15,6 +15,38 @@ public class WelcomeActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("Loginstate","0");
         editor.commit();
+        
+        
+          button_clear = (Button)findViewById(R.id.bn_Clear);
+
+        details = (TextView)findViewById(R.id.details_textView);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        // "" is the default value that is already saved in file or shared Preference.
+
+        stringBuilder.append("Welcome " +sharedPreferences.getString("Username","")).append("\n");
+        stringBuilder.append("Entered Email ID is " +sharedPreferences.getString("Email","")).append("\n");
+        stringBuilder.append("Given Number Is " +sharedPreferences.getString("phone","")).append("\n");
+
+
+        //Below we are passing values to TEXTVIEW in WELCOME ACTIVITY.
+
+        details.setText(String.valueOf(stringBuilder));
+
+
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(WelcomeActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+                editor.clear();
+                editor.commit();
+
+            }
+        });
 
 
     }
